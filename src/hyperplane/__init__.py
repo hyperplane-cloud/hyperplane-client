e2b9ac9ff7fd7bb3e7c29ace3ccd98f28458111c
tzvi_hyperplane <tzvi@hyperplane.cloud>
2022-08-31 10:26:28 +0300

    Use hyperplane_definitions in hyperplane-dev

--- src/hyperplane/__init__.py
+++ src/hyperplane/__init__.py
@@ -1,8 +1,8 @@
 import os
 import sys
 from .secret_utils import get_secret, get_s3_credentials
-
 from .exec_utils import is_job_running_on_server
+from hyperplane_definitions import get_job_id
 
 OUTPUT_FILES_DIR = os.environ.get("HYPERPLANE_USER_OUTPUTS_DIR", os.getcwd())
 OUTPUT_FILES_DIR_ABS = os.environ.get("HYPERPLANE_USER_OUTPUTS_DIR_ABS", os.getcwd())
@@ -12,15 +12,6 @@ def get_env_param(env_param):
     if env_param.startswith("HYPERPLANE_"):
         return None
     return os.environ.get(env_param)
-
-
-def get_job_id():
-    job_id = os.environ.get('HYPERPLANE_JOB_ID')
-
-    if not job_id:
-        print("Warning: job_id is not set, returning None. If needed you can set it via the HYPERPLANE_JOB_ID environment variable")
-
-    return job_id
 
 
 def get_user_id():
@@ -34,8 +25,9 @@ def get_user_id():
 
 def report(analytics_str):
     if is_job_running_on_server():
-        sys.path.append("..")
-        from hyperplane_server_utils import report as report_from_server
+        if ".." not in sys.path:
+            sys.path.append("..")
+        from report_apm import report as report_from_server
         return report_from_server(analytics_str)
 
     else: # running locally 
